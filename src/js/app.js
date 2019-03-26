e34560cab4c5c7f9fe12fc869c6b53428dd7e761
Naufal Farras <mnaufalfarras.mail@gmail.com>
2019-03-26 10:19:33 +0700

    added responsive navbar slide out

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,29 @@
 var mySwiper = new Swiper(".swiper-container", {
-    // simulateTouch: false,
-    // autoHeight: true,
-    // pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: true
-    // },
-
-    // Navigation arrows
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     },
     effect: 'fade',
 });
+
+var mobileMenu = document.querySelector('.mobile-menu');
+var sidenav = document.querySelector('.primary-links');
+var overlay = document.querySelector('.overlay');
+
+
+// window.addEventListener('click', function (e) {
+//     // console.log(e.target.parentNode);
+//     if (sidenav.classList.contains('is-open') && e.target != sidenav && e.target.parentNode != sidenav) {
+//         sidenav.classList.toggle('is-open');
+//     }
+// });
+
+mobileMenu.addEventListener('click', function () {
+    sidenav.classList.toggle('is-open');
+    overlay.classList.toggle('is-open');
+});
+
+overlay.addEventListener('click', function() {
+    sidenav.classList.remove('is-open');
+    overlay.classList.remove('is-open');
+})
